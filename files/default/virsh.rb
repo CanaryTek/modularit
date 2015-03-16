3decd929371a9496519e693be364e755b654e302
Miguel Armas <kuko@canarytek.com>
2015-03-16 13:29:38 +0000

    Add timeout to ohai locking in ovirt nodes

--- files/default/virsh.rb
+++ files/default/virsh.rb
@@ -4,7 +4,7 @@ provides "virtualization/guest_list"
 
 list=Mash.new
 if not virtualization.nil? and virtualization[:role] == 'host'
-  status, stdout, stderr = run_command(:no_status_check => true, :command => "virsh list | egrep -v '^( Id|---)|^$'")
+  status, stdout, stderr = run_command(:no_status_check => true, :timeout => 60, :command => "virsh list | egrep -v '^( Id|---)|^$'")
   if status == 0
     stdout.each_line do |g|
       id, name, state = g.split
